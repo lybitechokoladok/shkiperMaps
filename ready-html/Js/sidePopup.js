33119eecfc9fc58061f13e658f208f0f1f546c81
lybitechokoladok <102763296+lybitechokoladok@users.noreply.github.com>
2023-10-09 21:47:57 +0300

    add animation

--- ready-html/Js/sidePopup.js
+++ ready-html/Js/sidePopup.js
@@ -1,21 +1,24 @@
 jQuery(document).ready(function () {
-	jQuery('.contact-btn').click(function(){
-			jQuery('#side-box').toggleClass('open-box');		
+		jQuery('.contact-btn').click(function(){
+			jQuery('#side-box').toggleClass('open-box');
+			document.getElementById("open-close-icon").style.transform = "rotate(180deg)";	
 		});
 		jQuery('.close-icon').click(function(){
-			jQuery('#side-box').removeClass('open-box');		
+			jQuery('#side-box').removeClass('open-box');
+			document.getElementById("open-close-icon").style.transform = "rotate(180deg)";		
 		});
 		
 		jQuery('.contact-side').click(function(){
-			jQuery('#conact-content').toggleClass('open-box');		
+			jQuery('#conact-content').toggleClass('open-box');
+			document.getElementById("open-close-icon").style.transform = "rotate(180deg)";		
 		});
 		
 		
 		
 		jQuery(document).click(function(e) {
 					if (jQuery(e.target).closest('#side-box').length === 0 && jQuery(e.target).closest('.contact-btn').length === 0) {
+						document.getElementById("open-close-icon").style.transform = "rotate(-360deg)";
 							jQuery("#side-box").removeClass("open-box");
 					}
-	
 			});
 		});
